f0f0b5ef55a96391f98f297efd3bee1cb9e00966
Oleksandr <oshengera@gmail.com>
2023-01-11 11:28:01 +0200

    add animated menu

--- src/js/modules/scrolling.js
+++ src/js/modules/scrolling.js
@@ -2,9 +2,40 @@ const scrolling = () => {
   let links = document.querySelectorAll('[href^="#"]'),
     speed = 0.3;
 
+  window.addEventListener('scroll', () => {
+    let toTop = document.documentElement.scrollTop;
+    console.log(toTop);
+
+    if (toTop < 750) {
+      removeActiveClass();
+      links[1].classList.add('menu__link-active');
+    } else if (toTop > 750 && toTop < 1900) {
+      removeActiveClass();
+      links[2].classList.add('menu__link-active');
+    } else if (toTop > 1900 && toTop < 3240) {
+      removeActiveClass();
+      links[3].classList.add('menu__link-active');
+    } else if (toTop > 3240 && toTop < 3650) {
+      removeActiveClass();
+      links[4].classList.add('menu__link-active');
+    } else if (toTop > 3650 && toTop < 4120) {
+      removeActiveClass();
+      links[5].classList.add('menu__link-active');
+    } else if (toTop > 4120) {
+      removeActiveClass();
+      links[6].classList.add('menu__link-active');
+    }
+  });
+
   links.forEach(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault();
+
+      removeActiveClass();
+
+      if (link.classList.contains('menu__link')) {
+        link.classList.add('menu__link-active');
+      }
 
       let withTop = document.documentElement.scrollTop,
         hash = link.getAttribute('href'),
@@ -31,6 +62,14 @@ const scrolling = () => {
       }
     });
   });
+
+  function removeActiveClass() {
+    links.forEach(link => {
+      if (link.classList.contains('menu__link')) {
+        link.classList.remove('menu__link-active');
+      }
+    });
+  }
 };
 
 export default scrolling;
